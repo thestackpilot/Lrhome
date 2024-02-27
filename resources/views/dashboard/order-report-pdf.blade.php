35c8df8300d4eee368a16324090cf7528b8b8a8f
Ahmed Qalbi <ahmed@ashtexsolutions.com>
2024-02-27 15:18:19 +0500

    Issue solved

--- resources/views/dashboard/order-report-pdf.blade.php
+++ resources/views/dashboard/order-report-pdf.blade.php
@@ -1,6 +1,12 @@
 @php
     use App\Http\Controllers\ConstantsController;
     use App\Http\Controllers\CommonController;
+    function formatKey ($key) {
+        $result = preg_replace('/([A-Z|0-9])/', ' $1', $key);
+        $result = trim($result);
+        $result = str_replace('_', ' ', $result);
+        return ucfirst(preg_replace('/([A-Z])\s(?=[A-Z])/', '$1', $result));
+    }
 @endphp
 <html>
 <head>
